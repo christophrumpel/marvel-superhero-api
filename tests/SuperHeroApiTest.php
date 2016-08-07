c803715227fd3e539fe89f4bf7f4aed28760ba5a
Christoph Rumpel <c.rumpel@kabsi.at>
2016-08-07 20:30:39 +0200

    Update dependencies

---
+++ tests/SuperHeroApiTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Rumpel\MarvelUniverse\Test;
+
+use Rumpel\MarvelUniverse\SuperheroApi;
+
+class SuperHeroApiTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $api;
+
+    public function setUp()
+    {
+    }
+
+}
